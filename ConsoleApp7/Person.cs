27b3da927bb638b5437e22aa39ccbd6be36f1b78
darke <darke@DESKTOP-TDTVL8F>
2023-03-01 21:38:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+namespace first
+
+{
+    internal class Person
+    {
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        private char gender;
+        public char Gender
+        {
+            get { return gender; }
+            set { if (value == 'm' || value == 'f' || value == 'M' || value == 'F' ) gender = value; }
+        }
+        public Person() 
+        { 
+
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + LastName + " " + DateOfBirth + " " + gender;
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите имя: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию:");
+            LastName = Console.ReadLine();
+            Console.WriteLine("Введите дату рождения в формате год-месяц-день: ");
+            DateOfBirth = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Введите пол f/m: ");
+            gender = char.Parse(Console.ReadLine());
+        }
+        public void Print()
+        {
+            Console.WriteLine("Имя:"+ Name+ " Фамилия:" +LastName+ " Возраст:" +Age(DateOfBirth)+ " Пол:" +gender);
+        }
+        public int Age(DateTime BDay)
+        {
+            int age = DateTime.Now.Year - BDay.Year;
+            if (DateTime.Now.Day < BDay.Day)
+            {
+                if (DateTime.Now.Month < BDay.Month)
+                {
+                    age--;
+                }
+            }
+            return age;
+        }
+    }
+}
+    
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество людей: ");
+            int m = int.Parse(Console.ReadLine());
+            Person[] people = new Person[m];
+            for (int i = 0; i < m; i++)
+            {
+                people[i] = new Person();
+                people[i].Input();
+            }
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine(people[i].Name);
+                Console.WriteLine(people[i].LastName);
+                Console.WriteLine(people[i].Age(people[i].DateOfBirth));
+                Console.WriteLine(people[i].Gender);
+            }
+
+        }
+    }
+}
